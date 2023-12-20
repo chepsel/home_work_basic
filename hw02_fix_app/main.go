75d071e0f825efc26e1c994b5c49729544833026
chepsel <chepsel23@gmail.com>
2023-12-20 21:36:20 +0300

    first try

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	var path string = "data.json"
+	var path string
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -19,12 +19,12 @@ func main() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
+	staff, err = reader.ReadJSON(path)
+	if err != nil {
+		fmt.Print(err)
+	}
 
 	printer.PrintStaff(staff)
 }
