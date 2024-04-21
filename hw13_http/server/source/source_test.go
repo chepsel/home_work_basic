812ec87cc393b972c7f55bdf2397f2a78f48046b
chepsel <chepsel23@gmail.com>
2024-04-21 14:52:09 +0300

    veintiuno

--- hw13_http/server/source/source_test.go
+++ hw13_http/server/source/source_test.go
@@ -40,40 +40,6 @@ func TestFileDB(t *testing.T) {
 	}
 }
 
-func TestDelete(t *testing.T) {
-	testCases := []struct {
-		input1 Animal
-		input2 string
-		desc   string
-	}{
-		{
-			desc: "check valid",
-			input1: Animal{
-				ID:     "Ignat",
-				Name:   "Выхухоль",
-				Age:    12,
-				Weight: 21,
-				Hight:  30,
-			},
-			input2: "Ignat",
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-			mu := sync.Mutex{}
-			storageFile = testFile
-			animals := make(map[string]Animal)
-			animals[tC.input2] = tC.input1
-			storage := &Storage{Animals: animals}
-			err := storage.Delete(tC.input2, &mu)
-			if err != nil {
-				t.Errorf("error")
-			}
-			time.Sleep(100 * time.Millisecond)
-		})
-	}
-}
-
 func TestGet(t *testing.T) {
 	testCases := []struct {
 		input1 Animal
@@ -108,6 +74,40 @@ func TestGet(t *testing.T) {
 			storage := &Storage{Animals: animals}
 			got, _ := storage.Get(tC.input2)
 			assert.Equal(t, tC.want, got)
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	testCases := []struct {
+		input1 Animal
+		input2 string
+		desc   string
+	}{
+		{
+			desc: "check valid",
+			input1: Animal{
+				ID:     "Ignat",
+				Name:   "Выхухоль",
+				Age:    12,
+				Weight: 21,
+				Hight:  30,
+			},
+			input2: "Ignat",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			mu := sync.Mutex{}
+			storageFile = testFile
+			animals := make(map[string]Animal)
+			animals[tC.input2] = tC.input1
+			storage := &Storage{Animals: animals}
+			err := storage.Delete(tC.input2, &mu)
+			if err != nil {
+				t.Errorf("error")
+			}
+			time.Sleep(100 * time.Millisecond)
 		})
 	}
 }
